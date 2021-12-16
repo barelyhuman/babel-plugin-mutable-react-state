3437e41d3187ba1f356ec460b58a22a67d41e3a3
Siddharth <ahoy@barelyhuman.dev>
2021-12-16 08:31:03 +0530

    tests: add for scope based and multiple components

--- test/test.ts
+++ test/test.ts
@@ -59,3 +59,79 @@ test('Check Functional Scope', (t) => {
 	}
 	t.snapshot(result.code)
 })
+
+test('Check Arrow Function Scope', (t) => {
+	const code = `
+    import * as React from "react";
+
+    let $b = 2;
+    
+    const Component = () => {
+      let $a = 1;
+    
+      const onPress = () => {
+        $a += 1;
+        $b = 3;
+      };
+    
+      return (
+        <div>
+          <p>{$a}</p>
+          <button onClick={onPress}>Press</button>
+        </div>
+      );
+    };
+    `
+
+	const result = compile(code)
+	if (!result) {
+		return t.fail()
+	}
+	t.snapshot(result.code)
+})
+
+test('Multi Component Scope', (t) => {
+	const code = `
+import * as React from "react";
+
+let $b = 2;
+
+const Component = () => {
+  let $a = 1;
+
+  const onPress = () => {
+    $a += 1;
+    $b = 3;
+  };
+
+  return (
+    <div>
+      <p>{$a}</p>
+      <button onClick={onPress}>Press</button>
+    </div>
+  );
+};
+
+const ComponentTwo = () => {
+  let $a = 3;
+
+  const onPress = () => {
+    $a = 5;
+    $b = 3;
+  };
+
+  return (
+    <div>
+      <p>{$a}</p>
+      <button onClick={onPress}>Press</button>
+    </div>
+  );
+};
+`
+
+	const result = compile(code)
+	if (!result) {
+		return t.fail()
+	}
+	t.snapshot(result.code)
+})
