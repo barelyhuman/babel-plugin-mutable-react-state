bacb536179ded9c20f222abfb554046b7ccb7ab0
Siddharth <ahoy@barelyhuman.dev>
2021-12-16 08:29:33 +0530

    add support for arrow functions

--- source/index.ts
+++ source/index.ts
@@ -10,144 +10,144 @@ export default function () {
 	return {
 		visitor: {
 			FunctionDeclaration(path: any) {
-				Object.keys(path.scope.bindings).forEach((binding) => {
-					if (/^\$/.test(binding)) {
-						const normName = normalizeName(binding)
-						// add to list of identifiers to compare and replace
-						// (not using scope replace to avoid shadow variables being replaced)
-						toMod.push({
-							raw: binding,
-							simplified: normName,
-						})
-					}
-				})
-
-				path.traverse({
-					Identifier({node}: {node: t.Identifier}) {
-						if (isReactiveIdentifier(node.name, toMod)) {
-							node.name = normalizeName(node.name)
-						}
-					},
-					VariableDeclaration({node}: {node: t.VariableDeclaration}) {
-						for (let i = 0; i < node.declarations.length; i += 1) {
-							const declaration = node.declarations[i]
-
-							if (
-								!(
-									t.isIdentifier(declaration.id) &&
-									/^\$/.test(declaration.id.name)
-								)
-							) {
-								continue
-							}
-
-							// change to const if it's `let` by any chance
-							node.kind = 'const'
-
-							const normName = normalizeName(declaration.id.name)
-							const setterName = getSetterName(normName)
-
-							// convert to `const [x,setX] = React.useState()`
-							node.declarations[i] = t.variableDeclarator(
-								t.arrayPattern([
-									t.identifier(normName),
-									t.identifier(setterName),
-								]),
-								t.callExpression(
-									t.memberExpression(
-										t.identifier('React'),
-										t.identifier('useState')
-									),
-									declaration.init ? [declaration.init] : []
-								)
-							)
-						}
-					},
-					ExpressionStatement({node}: {node: t.ExpressionStatement}) {
-						if (!t.isAssignmentExpression(node.expression)) {
-							return
-						}
-
-						//HACK: forced to assignment expression for now, will need to switch to a `switch`
-						// statement when working with both Assignment(=,+=,-=,etc) and Update expressions(++,--,**,etc)
-						const expression: t.AssignmentExpression = node.expression
-
-						if (!t.isIdentifier(expression.left)) {
-							return
-						}
-
-						if (!isReactiveIdentifier(expression.left.name, toMod)) {
-							return
-						}
-
-						const normName = normalizeName(expression.left.name)
-						const setterName = getSetterName(normName)
-
-						let callArgs: t.Expression[]
-
-						switch (expression.operator) {
-							case '=': {
-								callArgs = [{...expression.right}]
-								break
-							}
-
-							case '+=': {
-								callArgs = [
-									t.binaryExpression(
-										'+',
-										t.identifier(normName),
-										expression.right
-									),
-								]
-								break
-							}
-
-							case '-=': {
-								callArgs = [
-									t.binaryExpression(
-										'-',
-										t.identifier(normName),
-										expression.right
-									),
-								]
-								break
-							}
-
-							case '/=': {
-								callArgs = [
-									t.binaryExpression(
-										'/',
-										t.identifier(normName),
-										expression.right
-									),
-								]
-								break
-							}
-
-							case '*=': {
-								callArgs = [
-									t.binaryExpression(
-										'*',
-										t.identifier(normName),
-										expression.right
-									),
-								]
-								break
-							}
-							default: {
-								callArgs = []
-							}
-						}
-
-						node.expression = t.callExpression(
-							t.identifier(setterName),
-							callArgs
-						)
-					},
-				})
+				transformToStateByScope(path, toMod)
+			},
+			ArrowFunctionExpression(path: any) {
+				transformToStateByScope(path, toMod)
 			},
 		},
 	}
+}
+
+function transformToStateByScope(path: any, toMod: ToModifyVariableI[]) {
+	// TODO: check if the returned values are of the form `React.createElement`
+	// NOTE: can avoid the above one since custom hooks won't be able to use this
+
+	Object.keys(path.scope.bindings).forEach((binding) => {
+		if (/^\$/.test(binding)) {
+			// add to list of identifiers to compare and replace
+			// (not using scope replace to avoid shadow variables being replaced)
+			const normName = normalizeName(binding)
+			toMod.push({
+				raw: binding,
+				simplified: normName,
+			})
+		}
+	})
+
+	// nested traverse to avoid replacing bindings of anything other than what's in this
+	// function. To prevent creating state hooks outside a function
+	path.traverse({
+		Identifier({node}: {node: t.Identifier}) {
+			if (isReactiveIdentifier(node.name, toMod)) {
+				node.name = normalizeName(node.name)
+			}
+		},
+		VariableDeclaration({node}: {node: t.VariableDeclaration}) {
+			transformReactiveDeclarations(node, toMod)
+		},
+		ExpressionStatement({node}: {node: t.ExpressionStatement}) {
+			transformAssignmentExpression(node, toMod)
+		},
+	})
+}
+
+function transformReactiveDeclarations(
+	node: t.VariableDeclaration,
+	toMod: ToModifyVariableI[]
+) {
+	for (let i = 0; i < node.declarations.length; i += 1) {
+		const declaration = node.declarations[i]
+
+		if (
+			!(
+				t.isIdentifier(declaration.id) &&
+				isReactiveIdentifier(declaration.id.name, toMod)
+			)
+		) {
+			continue
+		}
+
+		// change to const if it's `let` by any chance
+		node.kind = 'const'
+
+		const normName = normalizeName(declaration.id.name)
+		const setterName = getSetterName(normName)
+
+		// convert to `const [x,setX] = React.useState()`
+		node.declarations[i] = t.variableDeclarator(
+			t.arrayPattern([t.identifier(normName), t.identifier(setterName)]),
+			t.callExpression(
+				t.memberExpression(t.identifier('React'), t.identifier('useState')),
+				declaration.init ? [declaration.init] : []
+			)
+		)
+	}
+}
+
+function transformAssignmentExpression(
+	node: t.ExpressionStatement,
+	toMod: ToModifyVariableI[]
+) {
+	if (!t.isAssignmentExpression(node.expression)) {
+		return
+	}
+	//HACK: forced to assignment expression for now, will need to switch to a `switch`
+	// statement when working with both Assignment(=,+=,-=,etc) and Update expressions(++,--,**,etc)
+	const expression: t.AssignmentExpression = node.expression
+
+	if (!t.isIdentifier(expression.left)) {
+		return
+	}
+
+	if (!isReactiveIdentifier(expression.left.name, toMod)) {
+		return
+	}
+
+	const normName = normalizeName(expression.left.name)
+	const setterName = getSetterName(normName)
+
+	let callArgs: t.Expression[]
+
+	switch (expression.operator) {
+		case '=': {
+			callArgs = [{...expression.right}]
+			break
+		}
+
+		case '+=': {
+			callArgs = [
+				t.binaryExpression('+', t.identifier(normName), expression.right),
+			]
+			break
+		}
+
+		case '-=': {
+			callArgs = [
+				t.binaryExpression('-', t.identifier(normName), expression.right),
+			]
+			break
+		}
+
+		case '/=': {
+			callArgs = [
+				t.binaryExpression('/', t.identifier(normName), expression.right),
+			]
+			break
+		}
+
+		case '*=': {
+			callArgs = [
+				t.binaryExpression('*', t.identifier(normName), expression.right),
+			]
+			break
+		}
+		default: {
+			callArgs = []
+		}
+	}
+
+	node.expression = t.callExpression(t.identifier(setterName), callArgs)
 }
 
 function isReactiveIdentifier(idName: string, modMap: ToModifyVariableI[]) {
